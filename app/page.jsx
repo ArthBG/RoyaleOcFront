03eadc4534667d3b1bdff62da89132a1fa4ab85a
Nicoly <nicolyvalferreira.564@gmail.com>
2023-11-27 17:14:12 -0300

    feat: colocando o footer na page jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,7 @@
 import Image from "next/image"
 import styles from "./page.module.css"
+import Link from "next/link"
+import Footer from "./components/footer/footer"
 
 export default function Home() {
   return (
@@ -8,6 +10,7 @@ export default function Home() {
       <div className={styles.containerLogo}>
         <Image src={"/images/logoclashroyale.png"} width={300} height={200} />
       </div>
+      <Footer />
     </main>
   )
 }
