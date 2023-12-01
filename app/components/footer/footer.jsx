4cb84cc10a602fa2e2770e2ac4fb4f7176d5cc84
Nicoly <nicolyvalferreira.564@gmail.com>
2023-12-01 15:31:34 -0300

    feat: finalização footer

--- app/components/footer/footer.jsx
+++ app/components/footer/footer.jsx
@@ -4,43 +4,50 @@ import Image from 'next/image';
 
 export const Footer = () => {
     return (
-        <footer className={styles.rodape}>
-            <div className={styles.rodapediv}>
+        <div>
+            <footer className={styles.rodape}>
+                <div className={styles.rodapediv}>
 
-                <div className={styles.rodapediv1}>
-                    <div className={styles.rodapediv1coluna}>
-                        <span><strong>LOGO</strong></span>
-                        <p>SIA Trecho 5 lote 000 bloco z sala 900 - Guará, Brasília - DF, 70000-010</p>
+                    <div className={styles.rodapediv1}>
+                        <div className={styles.rodapediv1coluna}>
+                            <span><strong className={styles.titulos}>Home</strong></span>
+                            <p className={styles.home}><Link href={'/'}>Navegue clicando aqui!</Link></p>
+                        </div>
                     </div>
-                </div>
 
-                <div className={styles.rodapediv2}>
-                    <div className={styles.rodapediv2coluna}>
-                        <span><strong>Contatos</strong></span>
-                        <p>contato@na.na</p>
-                        <p>+55 63 99200-0000</p>
+                    <div className={styles.rodapediv2}>
+                        <div className={styles.rodapediv2coluna}>
+                            <span><strong className={styles.titulos}>Contato</strong></span>
+                            <p>Entre em contato com nossa equipe:</p>
+                            <p className={styles.hovercarta}><Link href={'/contato.jsx'}><Image src={'/images/cartasclash.png'} width={45} height={45}></Image></Link></p>
+                        </div>
                     </div>
-                </div>
 
-                <div className={styles.rodapediv3}>
-                    <div className={styles.rodapediv3coluna}>
-                        <span><strong>Links</strong></span>
-                        <p><a href="#servicos" className={styles.link}>Serviços</a></p>
-                        <p><a href="#empresa">Empresa</a></p>
-                        <p><a href="#sobre">Sobre</a></p>
+                    <div className={styles.rodapediv3}>
+                        <div className={styles.rodapediv3coluna}>
+                            <span><strong className={styles.titulos}>Sobre Nós</strong></span>
+                            <p><Link href={'/sobrenos'}>Integrantes</Link></p>
+                            <p><Link href={'/sobrenos'}>Sobre</Link></p>
+                            <p><Link href={'/sobrenos'}>Se torne um integrante!</Link></p>
+                        </div>
                     </div>
-                </div>
 
-                <div className={styles.rodapediv4}>
-                    <div className={styles.rodapediv4coluna}>
-                        <span><strong>Outros</strong></span>
-                        <p>Políticas de Privacidade</p>
+                    <div className={styles.rodapediv4}>
+                        <div className={styles.rodapediv4coluna}>
+                            <span><strong className={styles.titulos}>Linktree</strong></span>
+                            <p>
+                                <Link href={'https://linktr.ee/joao.senai'} target={'parent'}>
+                                    <Image src={'/images/linktree.png'} width={50} height={50} className={styles.linktree}>
+                                    </Image>
+                                </Link>
+                            </p>
+                        </div>
                     </div>
-                </div>
 
-            </div>
-            <p className={styles.rodapedireitos}>Copyright © 2023, Todos os Direitos Reservados.</p>
-        </footer>
+                </div>
+                <p className={styles.rodapedireitos}>Octopus © 2023, Todos os Direitos Reservados.</p>
+            </footer>
+        </div>
     )
 }
 export default Footer;
