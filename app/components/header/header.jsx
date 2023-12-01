915e56c51ece77ec43c7ca0b64529ebc1602d989
João Victor <joaosantos.senai564@gmail.com>
2023-12-01 10:44:06 -0300

    header

--- app/components/header/header.jsx
+++ app/components/header/header.jsx
@@ -2,12 +2,13 @@ import styles from './header.module.css'
 
 
 export const Header = () => {
-    return(
-        <header>
-              <div className={styles.container}>
+    return (
+        <header className={styles.main}>
+
             <h1>sim</h1>
-        </div>
         </header>
-      
+
     )
 }
+
+export default Header;
--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Header from "./components/header/header"
 import styles from "./page.module.css"
 import Link from "next/link"
 import Footer from "./components/footer/footer"
@@ -6,6 +7,7 @@ import Footer from "./components/footer/footer"
 export default function Home() {
   return (
     <main className={styles.backgroundimage}>
+      <Header/>
       <h1>Clash Royale</h1>
       <div className={styles.containerLogo}>
         <Image src={"/images/logoclashroyale.png"} width={300} height={200} />
