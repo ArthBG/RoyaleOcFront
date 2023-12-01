10369701d4a9ec487ca475a4ec602d42a05c0a04
João Victor <joaosantos.senai564@gmail.com>
2023-12-01 10:31:25 -0300

    header

---
+++ app/components/header/header.jsx
@@ -0,0 +1,13 @@
+import styles from './header.module.css'
+
+
+export const Header = () => {
+    return(
+        <header>
+              <div className={styles.container}>
+            <h1>sim</h1>
+        </div>
+        </header>
+      
+    )
+}
