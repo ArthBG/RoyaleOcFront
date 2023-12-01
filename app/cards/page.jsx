8ef8ab5ca97adc66292f0f6b75f91e4726d673d8
arthurborges <arthurborges.senai564@gmail.com>
2023-12-01 13:42:26 -0300

    Fix Elixir select options

--- app/cards/page.jsx
+++ app/cards/page.jsx
@@ -109,16 +109,16 @@ function cardspage() {
                 </select>
                 <select className={style.select} onChange={(e) => setSelectedElixir(e.target.value)}>
                     <option value="all">Elixir:</option>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                    <option value="7">7</option>
-                    <option value="8">8</option>
-                    <option value="9">9</option>
-                    <option value ="10">10</option>
+                    <option value={1}>1</option>
+                    <option value={2}>2</option>
+                    <option value={3}>3</option>
+                    <option value={4}>4</option>
+                    <option value={5}>5</option>
+                    <option value={6}>6</option>
+                    <option value={7}>7</option>
+                    <option value={8}>8</option>
+                    <option value={9}>9</option>
+                    <option value ={10}>10</option>
                 </select>
                 <button className={style.scbtnred} onClick={clearFilters}>Redefinir Filtros</button>
                 </div>
