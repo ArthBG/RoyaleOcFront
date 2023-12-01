1f68d0be8f9f26a055e70d757b0d840a785c7310
João Victor <joaosantos.senai564@gmail.com>
2023-12-01 16:13:07 -0300

    header new

--- app/components/header/header.jsx
+++ app/components/header/header.jsx
@@ -1,18 +1,54 @@
 import styles from './header.module.css'
 import Image from 'next/image';
+import Link from 'next/link';
 
 
 export const Header = () => {
     return (
         <header className={styles.main}>
-            <div className={styles.icones}>
-            <Image className={styles.imagemain} src={'/images/coroaicone.png'} alt="Logo da empresa" width={60} height={60}></Image>
-            <h1 className={styles.title}>Royale.oc</h1>
-            </div>
-            <div className={styles.shortcut}>
-                
-            </div>
-        </header>
+        <div className={styles.container1}>
+        <Image className={styles.imagemain} src={'/images/coroaicone.png'} alt="Logo da empresa" width={60} height={60}></Image>
+        <h1 className={styles.title}>Royale.oc</h1>
+        </div>  
+        <div className={styles.container2}>
+        <div className={styles.shortcut1}>
+        //             <Link className={styles.a} href={'#'} target='_blank'>
+        //                 <Image className={styles.image} src={'/images/coroaicone.png'} width={45} height={45}></Image>
+        //                 <h1 className={styles.contact}>Contato</h1>
+        //             </Link>
+        //         </div>
+        </div>          
+    </header>
+
+
+        // <header className={styles.main}>
+        //     <div className={styles.icones}>
+        //         <Image className={styles.imagemain} src={'/images/coroaicone.png'} alt="Logo da empresa" width={60} height={60}></Image>
+        //         <h1 className={styles.title}>Royale.oc</h1>
+        //     </div>
+        //     <div className={styles.shortcut}>
+        //         <div className={styles.shortcut1}>
+        //             <Link className={styles.a} href={'#'} target='_blank'>
+        //                 <Image className={styles.image} src={'/images/coroaicone.png'} alt="Logo da empresa" width={45} height={45}></Image>
+        //                 <h1 className={styles.contact}>Contato</h1>
+        //             </Link>
+        //         </div>
+        //         <div className={styles.shortcut2}>
+        //             <Link className={styles.a} href={'#'} target='_blank'>
+        //                 <Image className={styles.image} src={'/images/coroaicone.png'} alt="Logo da empresa" width={45} height={45}></Image>
+        //                 <h1 className={styles.contact}>Contato</h1>
+        //             </Link>
+        //         </div>
+        //          <div className={styles.create}>
+        //      <Link className={styles.a} href={'#'} target='_blank'>
+        //          <div className={styles.buttoncr}>
+        //              <h1>sim</h1>
+        //          </div>
+        //      </Link>
+        //      </div>
+        //         </div>
+            
+        // </header>
 
     )
 }
