e648011430a5e893b33a2b7325359f76b15f48e6
João Victor <joaosantos.senai564@gmail.com>
2023-12-01 14:09:12 -0300

    title

--- app/components/header/header.jsx
+++ app/components/header/header.jsx
@@ -7,7 +7,10 @@ export const Header = () => {
         <header className={styles.main}>
             <div className={styles.icones}>
             <Image className={styles.imagemain} src={'/images/coroaicone.png'} alt="Logo da empresa" width={60} height={60}></Image>
-            <h1>sim</h1>
+            <h1 className={styles.title}>Royale.oc</h1>
+            </div>
+            <div className={styles.shortcut}>
+                
             </div>
         </header>
 
