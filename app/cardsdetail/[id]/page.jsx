0dd177e8ada7c30e736e801150349fd760a81427
isakamitech <isabelasouzade.564@gmail.com>
2023-12-02 11:37:35 -0300

    Add new dependencies and update card detail page carrousel

--- app/cardsdetail/[id]/page.jsx
+++ app/cardsdetail/[id]/page.jsx
@@ -3,10 +3,13 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import styles from './page.module.css';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import 'react-tabs/style/react-tabs.css';
 import Link from 'next/link';
 
-// detalhes da carta
-const cardDescription = ({ params }) => {
+const CardDescription = ({ params }) => {
     const [card, setCard] = useState([]);
     const { id } = params;
 
@@ -25,39 +28,36 @@ const cardDescription = ({ params }) => {
 
     return (
         <div className={styles.container}>
+            <Tabs>
+                <TabList>
+                    <Tab>Nome</Tab>
+                    <Tab>Imagem</Tab>
+                </TabList>
+
+                <TabPanel>
+                    <p className={styles.info}>{card.name ? card.name : 'Não disponível'}</p>
+
+                </TabPanel>
+                <TabPanel>
+                    <Carousel showArrows={true}>
+                        <div>
+                            <img src={"https://designdobom.com.br/wp-content/uploads/2018/09/imagem_para_sexta_51.jpg"} alt={`Imagem da Carta ${card.name}`} />
+                        </div>
+                        <div>
+                            <img src={"https://s2-techtudo.glbimg.com/SSAPhiaAy_zLTOu3Tr3ZKu2H5vg=/0x0:1024x609/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2022/c/u/15eppqSmeTdHkoAKM0Uw/dall-e-2.jpg"} alt={`Imagem da Carta ${card.name}`} />
+                        </div>
+                        <div>
+                            <img src={"https://img.freepik.com/fotos-premium/o-ceu-da-paisagem-do-arco-iris-refletiu-a-imagem-de-fundo-da-natureza-da-agua-ai-gerou-a-arte_856480-1354.jpg?w=2000"} alt={`Imagem da Carta ${card.name}`} />
+                        </div>
+                    </Carousel>
+
+                </TabPanel>
+            </Tabs>
 
             <div className={styles.infosCartas}>
                 <p className={styles.info}> {card.name ? card.name : "Não disponível"}</p>
                 <img src={card.image} alt={`Imagem da Carta ${card.name}`} />
 
-                <p className={styles.info}>Tipo de Carta: {card.type ? card.type : "Não disponível"}</p>
-                <p className={styles.info}>Raridade: {card.rarity ? card.rarity : "Não disponível"}</p>
-                <p className={styles.info}>Elixir: {card.elixir ? card.elixir : "Não disponível"}</p>
-                <p className={styles.info}>Pontos de Vida: {card.hp ? card.hp : "Não disponível"}</p>
-                <p className={styles.info}>Tempo de Geração: {card.deploytime ? card.deploytime : "Não disponível"}</p>
-                <p className={styles.info}>Pontos de Escudo: {card.shieldhp ? card.shieldhp : "Não disponível"}</p>
-                <p className={styles.info}>Descrição: {card.description ? card.description : "Não disponível"}</p>
-                <p className={styles.info}>Dano: {card.damage ? card.damage : "Não disponível"}</p>
-                <p className={styles.info}>Dano por Segundo: {card.damagepersecond ? card.damagepersecond : "Não disponível"}</p>
-                <p className={styles.info}>Dano à Distância: {card.damageondistance ? card.damageondistance : "Não disponível"}</p>
-                <p className={styles.info}>Dano em Área: {card.damageonarea ? card.damageonarea : "Não disponível"}</p>
-                <p className={styles.info}>Dano no Impacto: {card.damageonimpact ? card.damageonimpact : "Não disponível"}</p>
-                <p className={styles.info}>Dano na Torre: {card.damageontower ? card.damageontower : "Não disponível"}</p>
-                <p className={styles.info}>Dano da Carga: {card.chargedamage ? card.chargedamage : "Não disponível"}</p>
-                <p className={styles.info}>Dano de Morte: {card.damageondeath ? card.damageondeath : "Não disponível"}</p>
-                <p className={styles.info}>Velocidade de Invocação: {card.spawnspeed ? card.spawnspeed : "Não disponível"}</p>
-                <p className={styles.info}>Duração: {card.duration ? card.duration : "Não disponível"}</p>
-                <p className={styles.info}>Raio: {card.radius ? card.radius : "Não disponível"}</p>
-                <p className={styles.info}>Largura: {card.width ? card.width : "Não disponível"}</p>
-                <p className={styles.info}>Tempo de Efeito: {card.efecttime ? card.efecttime : "Não disponível"}</p>
-                <p className={styles.info}>Tempo de Congelamento: {card.freezetime ? card.freezetime : "Não disponível"}</p>
-                <p className={styles.info}>Quantidade de Unidades: {card.unities ? card.unities : "Não disponível"}</p>
-                <p className={styles.info}>Arena: {card.arena ? card.arena : "Não disponível"}</p>
-                <p className={styles.info}>Alvos: {card.target ? card.target : "Não disponível"}</p>
-                <p className={styles.info}>Alcance do Projétil: {card.projectilerange ? card.projectilerange : "Não disponível"}</p>
-                <p className={styles.info}>Alcance: {card.range ? card.range : "Não disponível"}</p>
-                <p className={styles.info}>Velocidade: {card.speed ? card.speed : "Não disponível"}</p>
-                <p className={styles.info}>Velocidade do Impacto: {card.impactspeed ? card.impactspeed : "Não disponível"}</p>
 
 
 
@@ -67,4 +67,4 @@ const cardDescription = ({ params }) => {
     );
 };
 
-export default cardDescription;
+export default CardDescription;
