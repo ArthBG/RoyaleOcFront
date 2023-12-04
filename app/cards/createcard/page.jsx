a800af6933f5f1676c341791064791533e8ea0b2
arthurborges <arthurborges.senai564@gmail.com>
2023-12-04 10:37:09 -0300

    Add styling for shabuya and scbtnyellow2 classes

--- app/cards/createcard/page.jsx
+++ app/cards/createcard/page.jsx
@@ -1,10 +1,26 @@
 "use client";
 import Image from "next/image"
-import ModalAtributes from "../../components/modalatributes/modalatributes";
+import stylesM from "../../components/modalatributes/modalatributes.module.css"
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 import axios from "axios"
 import { useState, useEffect } from "react"
 import styles from "../createcard/page.module.css"
 import CardInfo from "../../components/cardinfo/cardinfo";
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
 
 
 
@@ -43,6 +59,10 @@ export default function createCard() {
   const [speed, setSpeed] = useState("")
   const [impactspeed, setImpactspeed] = useState("")
   const [id, setId] = useState("")
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
 
   const handleImage = (e) => {
@@ -76,8 +96,126 @@ export default function createCard() {
     }
   }
 
-  const AtributesModal = () => {
-    ModalAtributes({ type })
+  const addAtributes = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await axios.post("/api/cards", {
+          hp,
+          deploytime,
+          shieldhp,
+          damage,
+          damagepersecond,
+          rangeddamage,
+          damageondistance,
+          damageonarea,
+          damageonimpact,
+          damageontower,
+          chargedamage,
+          damageondeath,
+          spawnspeed,
+          duration,
+          radius,
+          width,
+          efecttime,
+          freezetime,
+          unities,
+          arena,
+          target,
+          projectilerange,
+          range,
+          speed,
+          impactspeed
+        })
+      setHp("")
+      setDeploytime("")
+      setShieldhp("")
+      setDamage("")
+      setDamagepersecond("")
+      setRangeddamage("")
+      setDamageondistance("")
+      setDamageonarea("")
+      setDamageonimpact("")
+      setDamageontower("")
+      setChargedamage("")
+      setDamageondeath("")
+      setSpawnspeed("")
+      setDuration("")
+      setRadius("")
+      setWidth("")
+      setEfecttime("")
+      setFreezetime("")
+      setUnities("")
+      setArena("")
+      setTarget("")
+      setProjectilerange("")
+      setRange("")
+      setSpeed("")
+      setImpactspeed("")
+      console.log(response)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const removeAtributes = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await axios.post("/api/cards", {
+          hp,
+          deploytime,
+          shieldhp,
+          damage,
+          damagepersecond,
+          rangeddamage,
+          damageondistance,
+          damageonarea,
+          damageonimpact,
+          damageontower,
+          chargedamage,
+          damageondeath,
+          spawnspeed,
+          duration,
+          radius,
+          width,
+          efecttime,
+          freezetime,
+          unities,
+          arena,
+          target,
+          projectilerange,
+          range,
+          speed,
+          impactspeed
+        })
+      setHp("")
+      setDeploytime("")
+      setShieldhp("")
+      setDamage("")
+      setDamagepersecond("")
+      setRangeddamage("")
+      setDamageondistance("")
+      setDamageonarea("")
+      setDamageonimpact("")
+      setDamageontower("")
+      setChargedamage("")
+      setDamageondeath("")
+      setSpawnspeed("")
+      setDuration("")
+      setRadius("")
+      setWidth("")
+      setEfecttime("")
+      setFreezetime("")
+      setUnities("")
+      setArena("")
+      setTarget("")
+      setProjectilerange("")
+      setRange("")
+      setSpeed("")
+      setImpactspeed("")
+      console.log(response)
+    } catch (error) {
+      console.log(error)
+    }
   }
   
 
@@ -154,8 +292,136 @@ export default function createCard() {
 
         <input type = "file" className={styles.file} onChange={handleImage} />
         <textarea className={styles.input} placeholder="Descrição da carta" value={description} onChange={e => setDescription(e.target.value)} />
-        <button className={styles.scbtnyellow} onClick={AtributesModal}>Adicionar atributos</button> 
         <button className={styles.scbtnyellow} onClick={handleSubmit}>Criar</button>
+        {
+      type == "Tropa" ? (
+        <div className={styles.shabuya}>
+          <button className={styles.scbtnyellow2} onClick={handleOpen}>Adicionar Atributos</button>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Atributos para tropas
+              </Typography>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                <select>
+                <option value="">Selecione os atributos</option>
+                <option value="Pontos de Vida"><img src={"/images/hp.png"}/>Pontos de Vida</option>
+                <option value="Dano"><img src={"/images/dano.png"}/>Dano</option>
+                <option value="Dano por segundo<"><img src={"/images/danosegundo.png"}/>Dano por segundo</option>
+                <option value="Dano a distância"><img src={"/images/danoadistancia.png"}/>Dano a distância</option>
+                <option value="Dano de longa distância"><img src={"/images/danoaodistancia.png"}/>Dano de longa distância</option>
+                <option value="Dano em área"><img src={"/images/danoarea.png"}/>Dano em área</option>
+                <option value="Dano no impacto"><img src={"/images/danoimpacto.png"}/>Dano no impacto</option>
+                <option value="Dano na torre"><img src={"/images/danotorre.png"}/>Dano na torre</option>
+                <option value="Dano carregado"><img src={"/images/danocarregado.png"}/>Dano carregado</option>
+                <option value="Dano na morte"><img src={"/images/danomorte.png"}/>Dano na morte</option>
+                <option value="Velocidade de mobilização"><img src={"/images/velocidadespawn.png"}/>Velocidade de spawn</option>
+                <option value="Duração"><img src={"/images/duracao.png"}/>Duração</option>
+                <option value="Tempo de congelamento"><img src={"/images/tempocongelamento.png"}/>Tempo de congelamento</option>
+                <option value="Unidades"><img src={"/images/unidades.png"}/>Unidades</option>
+                <option value="Alvo"><img src={"/images/alvo.png"}/>Alvo</option>
+                <option value="Alcance do projétil"><img src={"/images/alcanceprojeto.png"}/>Alcance do projétil</option>
+                <option value="Alcance"><img src={"/images/alcance.png"}/>Alcance</option>
+                <option value="Velocidade"><img src={"/images/velocidade.png"}/>Velocidade</option>
+                <option value="Velocidade de impacto"><img src={"/images/velocidadeimpacto.png"}/>Velocidade de impacto</option>
+                <option value="Pontos de vida do escudo"><img src={"/images/pontosvidaescudo.png"}/>Pontos de vida do escudo</option>
+                </select>
+              </Typography>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                <input type="text" className={stylesM.atributos} placeholder="Valor do atributo" />
+              </Typography>
+              <div className={stylesM.containerButtons}>
+                <button className={stylesM.scbtnyellow} onClick={addAtributes}>Adicionar atributo</button>
+                <button className={stylesM.scbtnyellow} onClick={removeAtributes}>Remover atributo</button>
+              </div>
+            </Box>
+          </Modal>
+        </div>
+      ) : (
+        type == "Construção" ? (
+          <div className={styles.shabuya}>
+          <button className={styles.scbtnyellow2} onClick={handleOpen}>Adicionar Atributos</button>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Atributos para construção
+              </Typography>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                <select>
+                <option value="">Selecione os atributos</option>
+                <option value="Pontos de Vida"><img src={"/images/hp.png"}/>Pontos de Vida</option>
+                <option value="Dano"><img src={"/images/dano.png"}/>Dano</option>
+                <option value="Dano por segundo<"><img src={"/images/danosegundo.png"}/>Dano por segundo</option>
+                <option value="Dano a distância"><img src={"/images/danoadistancia.png"}/>Dano a distância</option>
+                <option value="Dano de longa distância"><img src={"/images/danoaodistancia.png"}/>Dano de longa distância</option>
+                <option value="Dano em área"><img src={"/images/danoarea.png"}/>Dano em área</option>
+                <option value="Dano na torre"><img src={"/images/danotorre.png"}/>Dano na torre</option>
+                <option value="Dano carregado"><img src={"/images/danocarregado.png"}/>Dano carregado</option>
+                <option value="Dano na morte"><img src={"/images/danomorte.png"}/>Dano na morte</option>
+                <option value="Velocidade de mobilização"><img src={"/images/velocidadespawn.png"}/>Velocidade de spawn</option>
+                <option value="Duração"><img src={"/images/duracao.png"}/>Duração</option>
+                <option value="Unidades"><img src={"/images/unidades.png"}/>Unidades</option>
+                <option value="Alvo"><img src={"/images/alvo.png"}/>Alvo</option>
+                <option value="Alcance"><img src={"/images/alcance.png"}/>Alcance</option>
+                  </select>
+                </Typography>
+                <div className={stylesM.containerButtons}>
+                <button className={stylesM.scbtnyellow} onClick={addAtributes}>Adicionar atributo</button>
+                <button className={stylesM.scbtnyellow} onClick={removeAtributes}>Remover atributo</button>
+              </div>
+            </Box>
+          </Modal>
+        </div>
+      ) : (
+        type == "Feitiço" ? (
+          <div className={styles.shabuya}>
+          <button className={styles.scbtnyellow2} onClick={handleOpen}>Adicionar Atributos</button>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Atributos para feitiços
+              </Typography>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                <select>
+                <option value="">Selecione os atributos</option>
+                <option value="Dano em área"><img src={"/images/danoarea.png"}/>Dano em área</option>
+                <option value="Dano na torre"><img src={"/images/danotorre.png"}/>Dano na torre</option>
+                <option value="Duração"><img src={"/images/duracao.png"}/>Duração</option>
+                <option value="Tempo de congelamento"><img src={"/images/tempocongelamento.png"}/>Tempo de congelamento</option>
+                <option value="Unidades"><img src={"/images/unidades.png"}/>Unidades</option>
+                <option value="Alcance"><img src={"/images/alcance.png"}/>Alcance</option>
+                <option value="Alvo"><img src={"/images/alvo.png"}/>Alvo</option>
+                <option value="Raio"><img src={"/images/alcanceprojeto.png"}/>Raio</option>
+                <option value="Paralisação"><img src={"/images/tempocongelamento.png"}/>Paralisação</option>
+  
+                  </select>              
+              </Typography>
+              <div className={stylesM.containerButtons}>
+                <button className={stylesM.scbtnyellow} onClick={addAtributes}>Adicionar atributo</button>
+                <button className={stylesM.scbtnyellow} onClick={removeAtributes}>Remover atributo</button>
+              </div>
+            </Box>
+          </Modal>
+        </div>
+      ) : null
+      )
+      ) 
+        } 
       </div>
 
 
