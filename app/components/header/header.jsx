a5580d3994e4e4cbdebad1afe61b2c5d837faf34
matheuscosta <matheuszambon564@gmail.com>
2023-12-04 16:41:57 -0300

    Refactor header component

--- app/components/header/header.jsx
+++ app/components/header/header.jsx
@@ -7,33 +7,31 @@ export const Header = () => {
     return (
         <header className={styles.main}>
             <div className={styles.container1}>
-                <Link className={styles.a} href={'/'} target='_parent'>
-                    <Image className={styles.imagemain} src={'/images/coroaicone.png'} alt="Logo da empresa" width={60} height={60}></Image>
-                   
-                </Link>
+                <Image className={styles.imagemain} src={'/images/coroaicone.png'} alt="Logo da empresa" width={60} height={60}></Image>
+                <h1 className={styles.title}>Royale.oc</h1>
             </div>
             <div className={styles.container2}>
 
-                <div className={styles.divbutton1}>
-                    <Link className={styles.a2} href={'/createcard'} target='_parent'>
-                        Criar cartas
-                    </Link>
+            <div className={styles.divbutton1}>
+                <Link className={styles.a2} href={'/cards/createcard'} target='_blank'>
+                 Criar cartas 
+                </Link>
                 </div>
                 <div className={styles.divbutton2}>
                     <div className={styles.shortcut1}>
-                        <Link className={styles.a} href={'/membros'} target='_parent'>
+                        <Link className={styles.a} href={'/membros'} target='_blank'>
                             <Image className={styles.image} src={'/images/sobrenosescudo.png'} width={60} height={40}></Image>
                             <h1 className={styles.contact}>Sobre Nós</h1>
                         </Link>
                     </div>
                     <div className={styles.shortcut2}>
-                        <Link className={styles.a} href={'/contato'} target='_parent'>
+                        <Link className={styles.a} href={'/contato'} target='_blank'>
                             <Image className={styles.image1} src={'/images/contatoscr.png'} width={45} height={40}></Image>
                             <h1 className={styles.contact2}>Contato</h1>
                         </Link>
                     </div>
                 </div>
-
+                
             </div>
         </header>
 
