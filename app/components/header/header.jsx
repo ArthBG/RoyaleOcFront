fdb424d71b50c284d67083d7769bb5524d8686f3
Arthur Borges <123407087+Arthursenai@users.noreply.github.com>
2023-12-05 11:25:38 -0300

    Fix header link styling

--- app/components/header/header.jsx
+++ app/components/header/header.jsx
@@ -7,13 +7,15 @@ export default function Header (){
     return (
         <header className={styles.main}>
             <div className={styles.container1}>
+            <Link className={styles.a3} href={'/cards'} target='_parent'>
                 <Image className={styles.imagemain} src={'/images/coroaicone.png'} alt="Logo da empresa" width={60} height={60}></Image>
                 <h1 className={styles.title}>Royale.oc</h1>
+                </Link>
             </div>
             <div className={styles.container2}>
 
             <div className={styles.divbutton1}>
-            <Link className={styles.a2} href={'/cards/createcard'} target='_parent'>
+            <Link className={styles.a2} href={'/cards/createcard'}>
                  Criar cartas 
                 </Link>
                 </div>
