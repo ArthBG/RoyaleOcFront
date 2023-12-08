971ca49ae8ea61ebb1acdd7009f82deae20e4f69
matheuscosta <matheuszambon564@gmail.com>
2023-12-08 10:07:28 -0300

    Update footer and add game description

--- app/page.jsx
+++ app/page.jsx
@@ -7,11 +7,23 @@ import Footer from "./components/footer/footer"
 export default function Home() {
   return (
     <main className={styles.backgroundimage}>
-      <Header/>
+      <Header />
       <h1>Clash Royale</h1>
       <div className={styles.containerLogo}>
         <Image src={"/images/logoclashroyale.png"} width={300} height={200} />
       </div>
+      <div className={styles.container}>
+        <h2>O que é:</h2>
+        <Image src={"/images/"} width={300} height={200} />
+        <p>
+          Clash Royale é um jogo eletrônico freemium desenvolvido e publicado
+          pela Supercell, uma empresa da Finlândia. O jogo combina elementos de
+          jogos de cartas colecionáveis, defesa de torre e multiplayer online de
+          batalha. O jogo foi lançado globalmente em 2 de março de 2016.
+        </p>
+
+      </div>
+      
       <Footer />
     </main>
   )
