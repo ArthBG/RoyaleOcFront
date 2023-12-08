17aeab306d3669bd505c67fbb9476557fde62918
João Victor <joaosantos.senai564@gmail.com>
2023-12-08 17:10:48 -0300

    fim do dia

--- app/contatos/cadastroCtt/page.jsx
+++ app/contatos/cadastroCtt/page.jsx
@@ -9,16 +9,76 @@ import Footer from '../../components/footer/footer.jsx';
 export default function Forms() {
     const [nome, setNome] = useState("");
     const [email, setEmail] = useState("");
+    const [msg, setMsg] = useState("");
     const [telefone, setTelefone] = useState("");
     const [comentario, setComentario] = useState("");
     const [contatos, setContatos] = useState([]);
 
+    const [errorNome, setErrorNome] = useState("");
+    const [errorEmail, setErrorEmail] = useState("");
+    const [errorTelefone, setErrorTelefone] = useState("");
+    const [errorComentario, setErrorComentario] = useState("");
+
     const handleSubmit = async (e) => {
         e.preventDefault();
 
+        if(nome == "") {
+            setErrorNome("Preencha o campo corretamente!");
+            setTimeout(() => {
+                setErrorNome("");
+            }, 3000);
+            return;
+        } else if (nome.length <2) {
+            setErrorNome("É preciso de no mínimo 2 caracteres!");
+            setTimeout(() => {
+                setErrorNome("");
+        }, 3000);
+        return;
+    }
+        if(!email.includes('@')) {
+            setErrorEmail("Escreva um email válido!");
+            setTimeout(() => {
+                setErrorEmail("");
+            }, 3000);
+            return;
+        } else if (email.length <5) {
+            setErrorEmail("Escreva um email válido!");
+            setTimeout(() => {
+                setErrorEmail("");
+        }, 3000);
+        return;
+    } else if (email == "") {
+        setErrorEmail("Preencha o campo corretamente!");
+        setTimeout(() => {
+            setErrorEmail("");
+    }, 3000);
+    return;
+    } if (telefone == "") {
+        setErrorTelefone("Preencha o campo corretamente!");
+        setTimeout(() => {
+            setErrorTelefone("");
+    }, 3000);
+    return;
+}       if(comentario < 10) {
+    setErrorComentario("O comentário é de no mínimo 10 caracteres!");
+    setTimeout(() => {
+        setErrorComentario("");
+    }, 3000);
+    return;
+} else if (comentario == "") {
+    setErrorComentario("Preencha o campo corretamente!");
+    setTimeout(() => {
+        setErrorComentario("");
+}, 3000);
+return;
+}
+
+
+
+
         try {
             // Envia uma requisição POST para a API com os dados do novo membro.
-            await axios.post("/api/contatos", { nome, email, telefone, comentario });
+            await axios.post("/api/contatos", { nome, email, telefone, comentario});
             // Limpa os campos do formulário 
             setNome("");
             setEmail("");
@@ -27,6 +87,8 @@ export default function Forms() {
         } catch (error) {
 
             console.error("Error submitting data:", error);
+
+            setMsg("Erro ao cadastrar membro. Por favor, preencha todos os campos corretamente.");
             // Exibe mensagem de erro
 
         }
@@ -69,7 +131,7 @@ export default function Forms() {
                             onChange={(e) => setNome(e.target.value)}
                            
                         />
-
+                        <p>{errorNome}</p>
                     </div>
 
                     <div>
@@ -81,7 +143,7 @@ export default function Forms() {
                             onChange={(e) => setEmail(e.target.value)}
                            
                         />
-
+                        <p>{errorEmail}</p>
                     </div>
 
                     <div>
@@ -94,7 +156,7 @@ export default function Forms() {
                             onChange={(e) => setTelefone(e.target.value)}
                            
                         />
-
+                         <p>{errorTelefone}</p>
                     </div>
 
                     <div>
@@ -107,17 +169,15 @@ export default function Forms() {
                             onChange={(e) => setComentario(e.target.value)}
                            
                         />
-
+                         <p>{errorComentario}</p>
                     </div>
 
-                    <Link href="/contatos">
                     <button type="submit">
                         <div></div>
                         <div></div>
                         <div></div>
                         <span>Cadastrar</span>
                     </button >
-                    </Link>
 
                 </form >
             </div>
--- app/contatos/page.jsx
+++ app/contatos/page.jsx
@@ -11,7 +11,6 @@ import Footer from "../components/footer/footer";
 export default function Page() {
     // Estados para armazenar a lista de membros e dados adicionais.
     const [contatos, setContatos] = useState([]);
-    const [dados, setDados] = useState([]);
     const router = useRouter();
 
     // Função para deletar um comentario.
@@ -19,15 +18,14 @@ export default function Page() {
         const url = `/api/contatos/${id}`;
         try {
             await axios.delete(url);
-            setDados(dados.filter((contato) => contato.id !== id));
             setContatos(contatos.filter((contato) => contato.id !== id));
         } catch (error) {
             console.error("Error deleting contato", error);
         }
     };
-    console.log(dados, contatos)
+    console.log(contatos)
 
-    
+
     const update = (id) => {
         router.push(`/contatos/${id}`);
     };
@@ -39,7 +37,6 @@ export default function Page() {
                 // Atualiza os estados com a lista de membros e dados adicionais.
                 const response = await axios.get("/api/contatos");
                 setContatos(response.data.contatos);
-                setDados(response.data.contatos);
             } catch (error) {
                 console.error("Error fetching data:", error);
             }
@@ -47,30 +44,27 @@ export default function Page() {
 
         fetchContatos();
     }, []);
+
+    console.log({ contatos })
     // Renderização do componente.
     return (
 
         <div>
             <Header />
 
-            <h1>Contatos</h1>
+            <h1>comentario enviado com sucesso</h1>
 
-            <Link href="/contatos/cadastroCtt">
-                <button type="submit">
-                    Voltar
-                </button >
-            </Link>
+            <h1>Seus comentários</h1>
+
+
 
 
 
             <div>
-
-                {/* Se há dados, exibe a lista de membros; caso contrário, exibe uma mensagem de carregamento ou erro. */}
-                {dados && dados.length !== 0 ? (
-                    // Se há membros, mapeia e exibe as informações de cada membro.
+                <div>
                     <div>
-                        <div>
-                            {contatos.map((contato) => (
+                        {
+                            contatos && contatos.map((contato) => (
                                 <div key={contato.id}>
 
                                     <div>
@@ -80,17 +74,13 @@ export default function Page() {
                                             <strong></strong> {contato.nome}
                                         </p>
                                         <p>
-                                            <strong>Idade:</strong> {contato.idade}
+                                            <strong>Idade:</strong> {contato.email}
                                         </p>
                                         <p>
-                                            <strong></strong> {contato.cargo}
+                                            <strong></strong> {contato.telefone}
                                         </p>
                                         <p>
-                                            <strong></strong>
-                                            <img src={contato.foto} width={200} height={200} />
-                                        </p>
-                                        <p>
-                                            <strong>Descrição:</strong> {contato.descricao}
+                                            <strong>Descrição:</strong> {contato.comentario}
                                         </p>
 
                                         <div>
@@ -107,24 +97,25 @@ export default function Page() {
                                     </div>
 
                                 </div>
-
                             ))}
-
-                        </div>
-                        <div>
-                            <Link href="/contatos/cadastroCtt">
-                                <button type="submit">
-                                    <div></div>
-                                    <div></div>
-                                    <div></div>
-                                    <span>Cadastro</span>
-                                </button >
-                            </Link>
-                        </div>
                     </div>
-                ) : (
-                    <p>carregando..</p>
-                )}
+                    <div>
+                        <Link href="/contatos/cadastroCtt">
+                            <button type="submit">
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                                <span>Cadastro</span>
+                            </button >
+                        </Link>
+
+                        <Link href="/contatos/cadastroCtt">
+                            <button type="submit">
+                                Voltar
+                            </button >
+                        </Link>
+                    </div>
+                </div>
             </div>
             <Footer />
         </div>
