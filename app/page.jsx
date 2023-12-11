2d67b11a4d2da102d28322d4ef41eb02d59ae824
matheuscosta <matheuszambon564@gmail.com>
2023-12-11 08:42:39 -0300

    Add carousel component to home page

--- app/page.jsx
+++ app/page.jsx
@@ -4,9 +4,17 @@ import Header from "./components/header/header"
 import styles from "./page.module.css"
 import Link from "next/link"
 import Footer from "./components/footer/footer"
+import Carousel from 'react-bootstrap/Carousel';
+import { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 export default function Home() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
   return (
     <main className={styles.backgroundimage}>
       <Header />
@@ -35,8 +43,151 @@ export default function Home() {
           </div>
         </div>
       </div>
+      <div>
+        <Carousel activeIndex={index} onSelect={handleSelect}>
+          <Carousel.Item>
+            <Image src={"/images/"} width={700} height={500} />
+            <Carousel.Caption>
+              <h3>Estádio Goblin </h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image src={"/images/"} width={700} height={500} />
+            <Carousel.Caption>
+              <h3>Fosso dos Ossos</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image src={"/images/"} width={700} height={500} />
+            <Carousel.Caption>
+              <h3>Torneio Bárbaro</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image src={"/images/"} width={700} height={500} />
+            <Carousel.Caption>
+              <h3>Vale dos Feitiços</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image src={"/images/"} width={700} height={500} />
+            <Carousel.Caption>
+              <h3>Oficina do Construtor</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image src={"/images/"} width={700} height={500} />
+            <Carousel.Caption>
+              <h3>Parqinho da P.E.K.K.A</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image src={"/images/"} width={700} height={500} />
+            <Carousel.Caption>
+              <h3>Arena Real</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image src={"/images/"} width={700} height={500} />
+            <Carousel.Caption>
+              <h3>Pico Congelado</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image src={"/images/"} width={700} height={500} />
+            <Carousel.Caption>
+              <h3>Arena da Selva</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image src={"/images/"} width={700} height={500} />
+            <Carousel.Caption>
+              <h3>Montanha do Porco</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image src={"/images/"} width={700} height={500} />
+            <Carousel.Caption>
+              <h3>Eletrovale</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image src={"/images/"} width={700} height={500} />
+            <Carousel.Caption>
+              <h3>Cidade Assombrada </h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image src={"/images/"} width={700} height={500} />
+            <Carousel.Caption>
+              <h3>Covil do Patifes</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image src={"/images/"} width={700} height={500} />
+            <Carousel.Caption>
+              <h3>Pico Sereno</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image src={"/images/"} width={700} height={500} />
+            <Carousel.Caption>
+              <h3>Mina do Mineiro</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image src={"/images/"} width={700} height={500} />
+            <Carousel.Caption>
+              <h3>A cozinha do Executor</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image src={"/images/"} width={700} height={500} />
+            <Carousel.Caption>
+              <h3>Cripta Real</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image src={"/images/"} width={700} height={500} />
+            <Carousel.Caption>
+              <h3>Santuário Silencioso</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image src={"/images/"} width={700} height={500} />
+            <Carousel.Caption>
+              <h3>Spa de Dragões </h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image src={"/images/"} width={700} height={500} />
+            <Carousel.Caption>
+              <h3>Campo de Treinamentos</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image src={"/images/"} width={700} height={500} />
+            <Carousel.Caption>
+              <h3>Festa Clash</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image src={"/images/"} width={700} height={500} />
+            <Carousel.Caption>
+              <h3>Panquecas</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image src={"/images/"} width={700} height={500} />
+            <Carousel.Caption>
+              <h3>Arena Lendária</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
 
-
+          
+        </Carousel>
+        
+      </div>
       <Footer />
     </main>
   )
