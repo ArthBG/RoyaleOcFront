0b440c400356d5d4136b28d6d4087fb630af85f0
Arthur Borges <123407087+Arthursenai@users.noreply.github.com>
2023-12-14 18:18:01 -0300

    Update title in metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'RoyaleOcto',
+  title: 'RoyaleOc',
   description: 'Biblioteca do Clash Royale by Octopus',
 }
 
